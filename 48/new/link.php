f5842250fe695a7a88586e549a7e70728b74c4c7
zxc455212 <zxc37984@gmail.com>
2018-04-10 10:46:31 +0800

    0410a

---
+++ 48/new/link.php
@@ -0,0 +1,4 @@
+<?php
+	$db = new PDO("mysql:host=localhost;dbname=msg","root","");
+    $db->exec("set names utf8");
+?>
---
+++ 48/new/loggin.php
@@ -0,0 +1,37 @@
+<?php
+	include_once('link.php');
+	session_start();
+	if(!isset($_SESSION['check'])){
+		$_SESSION['check'] = 0;
+		echo $_SESSION['check'];
+	}
+	
+	function err(){
+		$_SESSION['check'] +=1;
+		if($_SESSION['check']>=3){
+			header('location:err.html');
+			$_SESSION['check'] = 0;
+		}
+	}
+	
+	$user = $_POST['user'];
+	$pwd = $_POST['pwd'];
+	$ans = $_POST['ans'];
+	$txt =$_POST['txt'];
+	$sql = $db->prepare('select * from login where username=:user');
+	$sql->bindValue('user',$user);
+	$sql->execute();
+	$row = $sql->fetch(PDO::FETCH_ASSOC);
+	if($row == false){
+		err();
+		echo "<script>alert('帳號錯誤。');location.href='./login.php';</script>";
+	}else if($pwd != $row['password']){
+		err();
+		echo "<script>alert('密碼錯誤。');location.herf='./login.php';</script>";
+	}else if($txt != $ans){
+		err();
+		echo "<script>alert('驗證碼錯誤。');location.href='./login.php';</script>";
+	}else{
+		echo "登入成功";
+	}
+?>

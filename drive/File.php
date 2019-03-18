d0149518480429e74541a8c5dfbaeb253f5281ba
zxc455212 <zxc37984@gmail.com>
2019-03-18 14:00:51 +0800

    新增測試用圖片、做完單個檔案上傳、準備做多個檔案上傳、進度條

--- drive/File.php
+++ drive/File.php
@@ -1,3 +1,6 @@
 <?php
     $URL = file_get_contents('php://input','r');
     echo json_decode($URL);
+    $File = fopen('./UserFiles/'.json_decode($URL),'x');
+    fwrite($File,'test');
+    fclose($File);

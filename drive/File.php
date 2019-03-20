ba185ce06485393b068da5891a4346ec73d9e9b4
zxc455212 <zxc37984@gmail.com>
2019-03-20 10:16:00 +0800

    Add Commit English

--- drive/File.php
+++ drive/File.php
@@ -2,5 +2,5 @@
     $URL = file_get_contents('php://input','r');
     echo json_decode($URL);
     $File = fopen('./UserFiles/'.json_decode($URL),'x+');
-    fwrite($File,'test');
+    fwrite($File,'xxx');
     fclose($File);

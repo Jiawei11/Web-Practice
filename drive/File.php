44a9b5e821cf66ec7fbb0a2f7de2b68a51d26db7
zxc455212 <zxc37984@gmail.com>
2019-03-21 20:16:49 +0800

    Json

--- drive/File.php
+++ drive/File.php
@@ -1,6 +1,6 @@
 <?php
     $URL = file_get_contents('php://input','r');
-    echo $URL;
+    echo json_encode($URL);
     // $File = fopen('./UserFiles/'.json_encode($URL),'x+');
     // fwrite($File,'xxx');
     // fclose($File);

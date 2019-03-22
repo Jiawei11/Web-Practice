fb28af6b3d3ca588b6ea30d2e76f4cf2ef86d9fd
zxc455212 <zxc37984@gmail.com>
2019-03-23 00:13:25 +0800

    上傳成功，準備做拖曳上傳

--- drive/File.php
+++ drive/File.php
@@ -1,6 +1,8 @@
 <?php
     $URL = file_get_contents('php://input');
-    // var_dump(json_decode($URL));
-    // $File = fopen('./UserFiles/'.json_encode($URL),'x+');
-    // fwrite($File,'xxx');
-    // fclose($File);
+    
+    $JSON = json_decode($URL);
+    $File = fopen('./UserFiles/' . strval($JSON->fileName) . '.jpg','wb');
+    var_dump($File);
+    fwrite($File,base64_decode(explode(',',$JSON->src)[1]));
+    fclose($File);

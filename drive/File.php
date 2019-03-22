af8ea0e01d29bfa95cc825df44db8a66f9ce22a8
zxc455212 <zxc37984@gmail.com>
2019-03-22 20:41:53 +0800

    圖片處理完成，準備做上傳檔案

--- drive/File.php
+++ drive/File.php
@@ -1,6 +1,6 @@
 <?php
-    $URL = file_get_contents('php://input','r');
-    echo json_encode($URL);
+    $URL = file_get_contents('php://input');
+    // var_dump(json_decode($URL));
     // $File = fopen('./UserFiles/'.json_encode($URL),'x+');
     // fwrite($File,'xxx');
     // fclose($File);
